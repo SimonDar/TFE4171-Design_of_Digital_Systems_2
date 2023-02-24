03b287194d55d354c56c309fdffdae864aaa1118
TFE4171Spring2023 Group 14 <dds223s14@aurora.tele.ntnu.no>
2023-02-24 16:36:55 +0100

    Checkpoint, starting to work on databases

--- ex-2/partB/alu_tb.sv
+++ ex-2/partB/alu_tb.sv
@@ -12,11 +12,25 @@ module alu_tb();
 parameter NUMBERS = 10000;
 
 //make your vector here
+alu_data test_data[NUMBERS-1]; 
 
 //Make your loop here
+initial begin : data_gen
+	#20
+	
+	for (int i = 0; i < NUMBERS; i++)
+	begin
+		test_data[i] = new();
+		test_data[i].randomize();
+		test_data[i].get(a, b, op);
+
+		#20;	
+	end 
+
+end 
 
 //Displaying signals on the screen
-always @(posedge clk) 
+always @(posedge clk)
   $display($stime,,,"clk=%b a=%b b=%b op=%b r=%b",clk,a,b,op,r);
 
 //Clock generation
@@ -26,15 +40,19 @@ always #10 clk=~clk;
 alu dut (clk,a,b,op,r);
 
 //Make your opcode enumeration here
-
+enum {ADD, SUB, NOT, NAND, NOR, AND, OR, XOR} opcode;
 
 //Make your covergroup here
-
+covergroup alu_cg @(posedge clk);
+	cg_op : coverpoint op;
+	a_op : coverpoint a;
+	a_b_op : cross a, b;
+endgroup
 
 //Initialize your covergroup here
-
+alu_cg alu_cg_inst = new();
 
 //Sample covergroup here
-
+always @(posedge clk) alu_cg_inst.sample();
 
 endmodule:alu_tb
